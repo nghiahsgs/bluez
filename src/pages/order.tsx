45d5307f3358556f55df341087efd3620b57b2ac
NamNguyenVan <nam.nguyenvan1601@gmail.com>
2024-04-14 21:44:50 +0700

    add loading

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -72,7 +72,7 @@ const EditableCell: React.FC<EditableCellProps> = ({
 
 const Order: React.FC = () => {
   const [form] = Form.useForm();
-  const { data: orderData } = useGetOrder();
+  const { data: orderData, loading } = useGetOrder();
   const [data, setData] = useState(orderData || []);
   const [editingKey, setEditingKey] = useState("");
   const masterData = useRecoilValue(masterDataState);
@@ -333,6 +333,7 @@ const Order: React.FC = () => {
   return (
     <Form form={form} component={false}>
       <Table
+        loading={loading}
         components={{
           body: {
             cell: EditableCell,
